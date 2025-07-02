fe31db9868e39ed82e0086982746d748b78f2948
baiyin <17693374267@163.com>
2025-07-02 22:06:27 +0800

    chore: init project

---
+++ src/test/java/org/example/buckpal/BuckpalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.buckpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckpalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

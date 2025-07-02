210a7ed4068b28f3e3512dbe9e2a16a216666ae2
baiyin <17693374267@163.com>
2025-07-03 00:32:08 +0800

    feature: add business rule validate

--- src/main/java/org/example/buckpal/application/service/SendMoneyService.java
+++ src/main/java/org/example/buckpal/application/service/SendMoneyService.java
@@ -1,9 +1,11 @@
 package org.example.buckpal.application.service;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.example.buckpal.application.port.in.SendMoneyCommand;
 import org.example.buckpal.application.port.in.SendMoneyUseCase;
 import org.example.buckpal.application.port.out.UpdateAccountStatePort;
+import org.example.buckpal.domain.AccountId;
 
 
 @RequiredArgsConstructor
@@ -14,8 +16,14 @@ public class SendMoneyService implements SendMoneyUseCase {
     @Override
     public boolean sendMoney(SendMoneyCommand command) {
         // todo: validate business rule
+        requireAccountExists(command.getSourceAccountId());
+        requireAccountExists(command.getTargetAccountId());
         // todo: manipulate model state
         // todo: return output
         return false;
+    }
+
+    private void requireAccountExists(AccountId accountId) {
+        // todo: check if the account actually exists in the database
     }
 }

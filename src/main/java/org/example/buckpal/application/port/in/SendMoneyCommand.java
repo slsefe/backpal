08a5ac75c73966358eeaca29f5add43eadf95d9a
baiyin <17693374267@163.com>
2025-07-03 00:01:20 +0800

    feature: validate input

--- src/main/java/org/example/buckpal/application/port/in/SendMoneyCommand.java
+++ src/main/java/org/example/buckpal/application/port/in/SendMoneyCommand.java
@@ -1,4 +1,23 @@
 package org.example.buckpal.application.port.in;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.buckpal.domain.AccountId;
+import org.example.buckpal.domain.Money;
+
+@Getter
+@RequiredArgsConstructor
 public class SendMoneyCommand {
+
+    @NotNull
+    private final AccountId sourceAccountId;
+
+    @NotNull
+    private final AccountId targetAccountId;
+
+    @NotNull
+    @Positive
+    private final Money money;
 }
